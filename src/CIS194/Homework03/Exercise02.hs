707a3cc18d26f1ac4d4d30a46b3f06774e5b457c
Daniel Deng <sinogermany@gmail.com>
2019-08-25 20:59:05 +1000

    Daniel Deng - Week 03 - Rev04

--- src/CIS194/Homework03/Exercise02.hs
+++ src/CIS194/Homework03/Exercise02.hs
@@ -1,13 +1,14 @@
 module CIS194.Homework03.Exercise02 (localMaxima) where
 
 import Data.List
+import Data.Maybe
 
-isLocalMax :: Ord a => [a] -> Bool
-isLocalMax [x, y, z] | y > x && y > z = True
-isLocalMax _                          = False
+maybeLocalMax :: [Integer] -> Maybe Integer
+maybeLocalMax [x, y, z] | y > x && y > z = Just y
+maybeLocalMax _                          = Nothing
 
 window :: Int -> [a] -> [[a]]
 window n = filter ((>= n) . length) . fmap (take n) . tails
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = map (!!1) . filter isLocalMax . window 3
+localMaxima = mapMaybe maybeLocalMax . window 3
